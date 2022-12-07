1f1c0b66bf08992a8cddd2bcf32ff2e9fb2a1e38
ldelfabro <delfabro_leandro@hotmail.com>
2022-12-06 21:43:16 -0300

    Se crea un output para posible retorno del user registrado y la interfaz

--- src/app/Componentes_Usuarios/alta-usuario-sistema/alta-usuario-sistema.component.ts
+++ src/app/Componentes_Usuarios/alta-usuario-sistema/alta-usuario-sistema.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Persona } from 'src/app/Interfaces/IPersona';
 
 @Component({
   selector: 'app-alta-usuario-sistema',
@@ -11,10 +12,24 @@ export class AltaUsuarioSistemaComponent implements OnInit {
   public formularioPrincipal: FormGroup;
   public flagSubmit: boolean;
   public flagEstado: boolean;
+  
+  @Output() NuevoUsuarioRetorno = new EventEmitter<Persona>();
+  public NuevoUsuario : Persona;
+
 
   constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    
+    this.NuevoUsuario = {
+      id: 0,
+      nombre: '',
+      apellido : '',
+      fechaNacimiento : new Date(),
+      telefono : '',
+      email : '',
+      pass : ''
+    }
 
     this.flagSubmit = false;
     this.flagEstado = false;
@@ -31,13 +46,26 @@ export class AltaUsuarioSistemaComponent implements OnInit {
   }
   submit() : void {
     this.flagSubmit = true;
-    console.log(this.formularioPrincipal.value);
-    console.log(this.formularioPrincipal.valid);
-    console.log(this.formularioPrincipal);
+   // console.log(this.formularioPrincipal.value);
+   // console.log(this.formularioPrincipal.valid);
+   // console.log(this.formularioPrincipal);
     if(this.formularioPrincipal.valid){
+      this.NuevoUsuario.nombre = this.formularioPrincipal.get('nombre')?.value;
+      this.NuevoUsuario.apellido = this.formularioPrincipal.get('apellido')?.value;
+      this.NuevoUsuario.telefono = this.formularioPrincipal.get('telefono')?.value;
+      this.NuevoUsuario.email = this.formularioPrincipal.get('email')?.value;
+      this.NuevoUsuario.pass = this.formularioPrincipal.get('pass')?.value;
+      this.NuevoUsuario.fechaNacimiento = new Date(this.formularioPrincipal.get('fechaNacimiento')?.value);
+
+      console.log(this.NuevoUsuario);
+
       this.flagEstado = true;
     }
 
-
   }
+
+  volver() : void {
+    this.NuevoUsuarioRetorno.emit(this.NuevoUsuario);
+  }
+
 }
---
+++ src/app/Interfaces/IPersona.ts
@@ -0,0 +1,9 @@
+export interface Persona {
+    id : number,
+    nombre : string,
+    apellido : string,
+    telefono : string,
+    fechaNacimiento : Date,
+    pass : string,
+    email : string
+}
