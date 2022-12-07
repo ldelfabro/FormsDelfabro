f9f45529c6866af2fdb4f37d8a9aade260b50423
ldelfabro <delfabro_leandro@hotmail.com>
2022-12-06 21:29:29 -0300

    Se ordenan carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AltaUsuarioSistemaComponent } from './alta-usuario-sistema/alta-usuario-sistema.component';
-import { CabeceraComponent } from './cabecera/cabecera.component';
+import { AltaUsuarioSistemaComponent } from './Componentes_Usuarios/alta-usuario-sistema/alta-usuario-sistema.component';
+import { CabeceraComponent } from './Componentes_Usuarios/cabecera/cabecera.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
