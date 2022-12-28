e03d17b73f8d410bc0884924b143ef4f123e8501
ldelfabro <delfabro_leandro@hotmail.com>
2022-12-28 11:36:37 -0300

    Se agrega ejemplo de ngDestroy

--- src/app/Componentes/Layout/footer/footer.component.ts
+++ src/app/Componentes/Layout/footer/footer.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { CursoServiceService } from '../../../Services/curso-service.service';
 import { LocalidadService } from '../../../Services/localidad.service';
 
@@ -8,17 +8,26 @@ import { LocalidadService } from '../../../Services/localidad.service';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
 
   public fechaObservable: Observable<Date>;
+  private suscripcion : Subscription;
 
   constructor(public localidadService : LocalidadService) { }
 
   ngOnInit(): void {
 
     this.fechaObservable = this.localidadService.getFecha();
+    
+    this.suscripcion = this.localidadService.getFecha().subscribe(value => 
+      {
+        console.log("me estoy ejecutando");
+      })
 
+  }
 
+  ngOnDestroy(): void {
+    this.suscripcion.unsubscribe();
   }
 
 }
