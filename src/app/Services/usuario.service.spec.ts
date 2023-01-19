c10e94ac212ea7ce761c87f7198522629171c62d
Leandro Delfabro <ldelfabro@emmsa.net>
2023-01-19 20:19:29 -0300

    Se agrega testing en UsuarioService

--- src/app/Services/usuario.service.spec.ts
+++ src/app/Services/usuario.service.spec.ts
@@ -1,18 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UsuarioService } from './usuario.service';
+import { UsuarioServiceMock } from '../mocks/usuario.service.mock';
 
-describe('ProfesorService', () => {
+fdescribe('UsuarioService', () => {
   let service: UsuarioService;
+  let spyDesloguear : jasmine.Spy;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports : [HttpClientTestingModule ]
+      imports : [HttpClientTestingModule ],
+      providers: [
+        {
+          provide : UsuarioService,
+          useClass : UsuarioServiceMock
+        }
+      ]
     });
     service = TestBed.inject(UsuarioService);
+    spyDesloguear = spyOn(service, 'desloguear').and.callThrough()
+
   });
 
-  it('should be created', () => {
+  it('Se debe crear el service usuario', () => {
     expect(service).toBeTruthy();
+  });
+ 
+  it('Se debe desloguear', () => {
+    service.desloguear();
+    expect(spyDesloguear).toHaveBeenCalled();
+  });
+  it('Debe funcionar el cambio de logueo', () => {
+    service.setLogueado(true)
+    service.getLogueado().subscribe((result) => {
+        expect(result).toBeTrue()
+    });
+  });
+  it('Debe funcionar el logueo', () => {
+    service.loguear('test@test','test').subscribe((result) => {
+      expect(result.nombre).toBe("test")
+    });
   });
 });
--- src/app/mocks/usuario.service.mock.ts
+++ src/app/mocks/usuario.service.mock.ts
@@ -1,21 +1,28 @@
 import { HttpClient } from "@angular/common/http";
-import { BehaviorSubject, Observable, of } from "rxjs";
+import { BehaviorSubject, map, Observable, of } from "rxjs";
 import { Usuario } from "../Interfaces/IUsuario";
 
 export class UsuarioServiceMock {
 
     public data$ : Observable<Usuario[]>;
     private data = new BehaviorSubject<Usuario[]>([]);
-
+    public _logueado : boolean = false;
+    public logueado$ : Observable<boolean>;
+    private logueado = new BehaviorSubject<boolean>(false);
+  
     constructor() {
         this.data$ = this.data.asObservable();
         this.getAll().subscribe(alumnos => {
           this.data.next(alumnos);
         });
+
+        this.logueado$ = this.logueado.asObservable();
+        this.logueado.next(this._logueado);
+    
     }
 
     public getAll() : Observable<Usuario[]> {
-        return of([ { nombre : '', apellido : '',  id : 0, email : '', pass: '' }]);
+        return of([ { nombre : 'test', apellido : 'test',  id : 1, email : 'test@test', pass: 'test' }]);
     }
 
     public add(alumno : Usuario){
@@ -34,5 +41,17 @@ export class UsuarioServiceMock {
       this.data.next(nuevaLista);
     }
 
-
+    public setLogueado(logueado : boolean){
+      this.logueado.next(logueado);
+     }
+     public getLogueado() : Observable<boolean> {
+      return this.logueado.asObservable();
+     }
+     public desloguear(){
+      this.logueado.next(false);
+     }
+     public loguear(email : string, pass : string): Observable<Usuario> {
+      return this.getAll().pipe(map((value : Usuario[]) => value.filter(c => c.email == email && c.pass == pass)[0]));
+     }
+  
 }
