453e9d20356f790be7879dbca1465c1f98a35ead
Leandro Delfabro <ldelfabro@emmsa.net>
2023-01-19 16:00:01 -0300

    --

--- src/app/Componentes/ABMs/Alumno/lista-alumno/lista-alumno.component.spec.ts
+++ src/app/Componentes/ABMs/Alumno/lista-alumno/lista-alumno.component.spec.ts
@@ -2,14 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ListaAlumnoComponent } from './lista-alumno.component';
-import { AlumnoService } from 'src/app/Services/alumno.service';
-import { AlumnoServiceMock } from 'src/app/mocks/alumno.service.mock';
 
 describe('ListaAlumnoComponent', () => {
   let component: ListaAlumnoComponent;
   let fixture: ComponentFixture<ListaAlumnoComponent>;
-  let spyLoadData : jasmine.Spy;
-  let alumnoService : AlumnoService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ListaAlumnoComponent ],
@@ -21,15 +17,12 @@ describe('ListaAlumnoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListaAlumnoComponent);
     component = fixture.componentInstance;
-    
-    
+  
     fixture.detectChanges();
   });
 
   it('Se debe crear el componente ListaAlumnoComponent', () => {
     expect(component).toBeTruthy();
   });
-
-
 
 });
