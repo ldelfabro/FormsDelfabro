be6885ade50040ee0ecb043bca0043bd667bb169
Leandro Delfabro <ldelfabro@emmsa.net>
2023-01-24 20:03:48 -0300

    Se eliminan componentes obsoletos

--- src/app/Componentes/Layout/header/header.component.ts
+++ src/app/Componentes/Layout/header/header.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
-import { ModalRegistrarComponent } from '../modal-registrar/modal-registrar.component';
 import { Usuario } from 'src/app/Interfaces/IUsuario';
-import { ModalLoguearComponent } from '../modal-loguear/modal-loguear.component';
 import { UsuarioService } from 'src/app/Services/usuario.service';
 import { Router } from '@angular/router';
 
@@ -32,97 +30,6 @@ export class HeaderComponent implements OnInit {
     this.usuarioService.desloguear();
     this.router.navigate(['/login'])
 
-  }
-
-  Loguear() : void {
-
-    this.errorMessage = '';
-
-    var profesor : Usuario = {
-      nombre : "",
-      apellido : "",
-      email : "",
-      pass : "",
-      id : 0
-    }
-
-    const dialogRef = this.dialog.open(ModalLoguearComponent, {data :profesor});
-
-    dialogRef.afterClosed().subscribe(result => {
-      if(result != undefined){
-        var existe = false;
-        var existeIndex = 0;
-
-        this.Profesores.forEach(function(elemento, index){
-          if(elemento.pass == result.pass && elemento.email == result.email){
-            existeIndex = index;
-            existe = true;
-          }
-        });
-        if(existe)
-        {
-          this.Profesor = this.Profesores[existeIndex];
-          this.logueado = true;
-        }
-        else
-        {
-          this.logueado = false;
-          this.errorMessage = 'Nombre de usuario o password incorrecto/s';
-        }
-      }
-
-    });
-
-  }
-
-  Registrar () : void {
-
-    this.errorMessage = '';
-
-    var profesor : Usuario = {
-      nombre : "",
-      apellido : "",
-      email : "",
-      pass : "",
-      id : 0
-    }
-
-    const dialogRef = this.dialog.open(ModalRegistrarComponent, {data :profesor});
-
-    dialogRef.afterClosed().subscribe(result => {
-
-      if(result != undefined){
-
-        var existe = false;
-
-        this.Profesores.forEach(function(elemento, index){
-          if(elemento.email == result.email){
-            existe = true;
-          }
-        });
-
-        if(!existe)
-        {        
-          var nuevoProfesor : Usuario = {
-            apellido : result.apellido,
-            nombre : result.nombre,
-            pass : result.pass,
-            id : 0,
-            email : result.email
-          };
-          nuevoProfesor.id = this.Profesores.length;
-          this.Profesores.push(nuevoProfesor);
-        }
-        else
-        {
-          this.logueado = false;
-          this.errorMessage = 'Ya existe un usuario con ese correo';
-        }
-
-
-      }
-    });
-    
   }
 
 }
--- src/app/Componentes/Layout/layout-module.module.ts
+++ src/app/Componentes/Layout/layout-module.module.ts
@@ -6,8 +6,6 @@ import { LeftPanelComponent } from './left-panel/left-panel.component';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ModalRegistrarComponent } from './modal-registrar/modal-registrar.component';
-import { ModalLoguearComponent } from './modal-loguear/modal-loguear.component';
 import { PipeModuleModule } from 'src/app/Pipes/pipe-module.module';
 import { LayoutModuleRoutingModule } from './layout-module-routing.module';
 import { RouterModule } from '@angular/router';
@@ -15,8 +13,6 @@ import { PrimerEntregableComponent } from './Principal/principal.component';
 
 @NgModule({
   declarations: [
-    ModalRegistrarComponent,
-    ModalLoguearComponent,
     PrimerEntregableComponent,
     HeaderComponent,
     FooterComponent,
